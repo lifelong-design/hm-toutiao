f5df7d046b7deadbf684ef7b4bbafd7fefb19b89
liuxiao <1054105865@qq.com>
2019-08-01 22:13:38 +0800

    配置login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入路由
+import router from '@/router'
+// 导入样式
+import '@/styles/index.less'
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <el-card class="my-card"></el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scopend lang="less">
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.my-card {
+  height: 350px;
+  width: 450px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%);
+}
+</style>
