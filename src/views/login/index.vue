ec3b610913103e901e840302402958db3eab11ae
liuxiao <1054105865@qq.com>
2019-08-02 21:26:31 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,39 @@
 <template>
   <div class="container">
-    <el-card class="my-card"></el-card>
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt />
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入密码"
+            style="width:236px;margin-right:10px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-button type="primary" style="width:100%" @click="login">登录</el-button>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: { mobile: '17637615151', code: '246810' }
+    }
+  },
+  methods: {
+    login () {}
+  }
+}
 </script>
 
 <style scopend lang="less">
@@ -14,13 +42,18 @@ export default {}
   height: 100%;
   position: absolute;
   background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
-}
-.my-card {
-  height: 350px;
-  width: 450px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -60%);
+  .my-card {
+    height: 350px;
+    width: 400px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 30px;
+    }
+  }
 }
 </style>
