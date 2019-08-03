b10a93969bc391e773e0ff8bb6daf5be6cda0296
liuxiao <1054105865@qq.com>
2019-08-03 20:38:16 +0800

    首页完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,16 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 
+import Home from '@/views/home'
 import Login from '@/views/login'
+import Welcome from '@/views/welcome'
+
+import Article from '@/components/article'
+import Image from '@/components/image'
+import Publish from '@/components/publish'
+import Fans from '@/components/fans'
+import Comment from '@/components/comment'
+import Setting from '@/components/setting'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -10,6 +19,48 @@ const router = new VueRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/',
+      // name: 'home',
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'welcome',
+          component: Welcome
+        },
+        {
+          path: '/article',
+          name: 'article',
+          component: Article
+        },
+        {
+          path: '/image',
+          name: 'image',
+          component: Image
+        },
+        {
+          path: '/publish',
+          name: 'publish',
+          component: Publish
+        },
+        {
+          path: '/fans',
+          name: 'fans',
+          component: Fans
+        },
+        {
+          path: '/comment',
+          name: 'comment',
+          component: Comment
+        },
+        {
+          path: '/setting',
+          name: 'setting',
+          component: Setting
+        }
+      ]
     }
   ]
 })
